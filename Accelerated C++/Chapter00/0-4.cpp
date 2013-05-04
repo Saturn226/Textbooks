d75e9e93068328e931b8d6751899674ed5a584c1
Ashley Muller <serenehopes@msn.com>
2013-05-03 21:54:23 -0400

    Chapter00

---
+++ Accelerated C++/Chapter00/0-4.cpp
@@ -0,0 +1,13 @@
+//Hello World Program
+#include <iostream>
+
+int main(){
+
+	std::cout << "#include<iostream> \n\nint main(){ \n"
+	<<"std::cout << \"Hello World!\";\n"
+	<< "return 0;\n"
+"}";
+std::cin.ignore();
+return 0;
+
+}
